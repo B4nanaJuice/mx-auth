39cf35bea88bfbe35fb6d34dd2de0623c0e44564
B4nanaJuice <griesmaxime2@gmail.com>
2026-03-24 09:42:20 +0100

    Disabled modifications tracking

--- extensions/database.py
+++ extensions/database.py
@@ -15,6 +15,7 @@ db: SQLAlchemy = SQLAlchemy(model_class = Base)
 # Method for db connection
 def init_db(app: Flask) -> None:
     app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app = app)
 
     with app.app_context():
