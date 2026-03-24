603bd581766dba91afcb2c8485935e7ec45b4e16
B4nanaJuice <griesmaxime2@gmail.com>
2026-03-24 09:41:54 +0100

    Added public_id field

--- models/user.py
+++ models/user.py
@@ -8,10 +8,11 @@ class User(db.Model):
     __tablename__ = 'users'
 
     # Attributes
-    id: Mapped[int] = mapped_column(primary_key = True, autoincrement = True)
-    name: Mapped[str] = mapped_column(nullable = False)
-    email: Mapped[str] = mapped_column(nullable = False, unique = True)
-    password: Mapped[str] = mapped_column(nullable = False)
+    id: Mapped[int] = mapped_column(db.Integer, primary_key = True, autoincrement = True)
+    public_id: Mapped[str] = mapped_column(db.String(50), unique = True)
+    name: Mapped[str] = mapped_column(db.String(100), nullable = False)
+    email: Mapped[str] = mapped_column(db.String(70), nullable = False, unique = True)
+    password: Mapped[str] = mapped_column(db.String(80), nullable = False)
 
     def __repr__(self) -> str:
         return f'<User {self.name}>'
