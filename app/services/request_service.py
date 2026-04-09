aaadbc39108b3eee9580fbc5beef193b3faace2c
B4nanaJuice <griesmaxime2@gmail.com>
2026-04-09 08:31:16 +0200

    Created request service

---
+++ app/services/request_service.py
@@ -0,0 +1,20 @@
+# Imports
+import logging
+
+# Create logger
+logger = logging.getLogger(__name__)
+
+# Create custom exception class
+class RequestException(Exception):
+    def __init__(self, message: str, status_code: int = 400):
+        super().__init__(message)
+        self.message: str = message
+        self.status_code: int = status_code
+
+# Create static class
+class RequestService:
+    
+    # Check if redirect url is in allowed redirects
+    @staticmethod
+    def is_allowed_redirect(next_url: str) -> bool:
+        return True
