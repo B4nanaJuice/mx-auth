c2a024c1823f1819f79894e44b276dacf1e9d890
B4nanaJuice <57623972+B4nanaJuice@users.noreply.github.com>
2026-04-13 18:42:21 +0200

    Added CLI commands

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,14 @@ from flask import Flask, jsonify
 
 from config.settings import config
 from app.data.database import init_db
+from app.cli import register_commands
 
 def create_app() -> Flask:
     app = Flask(__name__)
 
     app.config.from_object(config)
     init_db(app = app)
+    register_commands(app = app)
 
     @app.get('/health')
     def get_health():
